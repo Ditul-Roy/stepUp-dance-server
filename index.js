cf7331c928de98f5c597f16046d19f2761572ac6
Ditul-Roy <ditulr@gmail.com>
2023-06-14 16:13:07 +0600

    some changed

--- index.js
+++ index.js
@@ -202,20 +202,20 @@ async function run() {
       res.send(result);
     })
 
-    // payment related section
+    // payment related section, this section i cannot work 
 
-    app.post('/create-payment-intent', async (req, res) => {
-      const { price } = req.body;
-      const amount = price * 100;
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount: amount,
-        currency: 'usd',
-        payment_method_types: ['card']
-      })
-      res.send({
-        clientSecret: paymentIntent.client_secret
-      })
-    })
+    // app.post('/create-payment-intent', async (req, res) => {
+    //   const { price } = req.body;
+    //   const amount = price * 100;
+    //   const paymentIntent = await stripe.paymentIntents.create({
+    //     amount: amount,
+    //     currency: 'usd',
+    //     payment_method_types: ['card']
+    //   })
+    //   res.send({
+    //     clientSecret: paymentIntent.client_secret
+    //   })
+    // })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
